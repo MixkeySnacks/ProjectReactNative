32f2bb252d934714262df059d807588c7567293f
MixkeySnacks <95331334+MixkeySnacks@users.noreply.github.com>
2022-01-26 10:21:38 +0700

    Add files via upload

---
+++ 1-26-2022/pages/SecondPage.js
@@ -0,0 +1,31 @@
+import { Text, View, Button, SafeAreaView } from 'react-native';
+import React from 'react';
+
+import {styles} from '../components/Styles'
+
+const SecondPage = ({navigation}) => {
+    return (
+        <SafeAreaView style={{flex:1}}>
+            <View style={{flex:1, padding:15}}>
+                <View style={styles.container}>
+                    <Text style={styles.textTopStyle}>
+                        This is the Second Page
+                    </Text>
+                    <Button
+                        title="GO TO FIRST PAGE"
+                        onPress={() => navigation.navigate('FirstPage')}
+                    />
+                    <Button
+                        title="GO TO THRID PAGE"
+                        onPress={() => navigation.navigate('ThirdPage')}
+                    />
+                </View>
+                <Text style={styles.textBottomStyle}>
+                    Thai-Nichi Institute of Technology
+                </Text>
+            </View>
+        </SafeAreaView>
+      );
+};
+
+export default SecondPage;
