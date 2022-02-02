ba17e2cdfa9587006b56e42fe2ea42ee2ace6b89
MixkeySnacks <95331334+MixkeySnacks@users.noreply.github.com>
2022-02-02 11:12:35 +0700

    Add files via upload

---
+++ 2-2-2022/pages/FirstPage.js
@@ -0,0 +1,31 @@
+import { Text, View, Button, SafeAreaView } from 'react-native';
+import React from 'react';
+
+import {styles} from '../components/Styles'
+
+const FirstPage = ({navigation}) => {
+  return (
+    <SafeAreaView style={{flex:1}}>
+        <View style={{flex:1, padding:15}}>
+            <View style={styles.container}>
+                <Text style={styles.textTopStyle}>
+                    This is the First Page
+                </Text>
+                <Button
+                    title="GO TO SECOND PAGE"
+                    onPress={() => navigation.navigate('SecondPage')}
+                />
+                <Button
+                    title="GO TO THIRD PAGE"
+                    onPress={() => navigation.navigate('ThirdPage')}
+                />
+            </View>
+            <Text style={styles.textBottomStyle}>
+                    Thai-Nichi Institute of Technology
+            </Text>
+        </View>
+    </SafeAreaView>
+  );
+};
+
+export default FirstPage;
